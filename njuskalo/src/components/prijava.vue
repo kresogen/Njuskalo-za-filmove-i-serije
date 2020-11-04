70ae336210de04ec064c3203360ef3d5640c26f6
kresogen <37085665+kresogen@users.noreply.github.com>
2020-11-04 19:06:32 +0100

    prijava

---
+++ njuskalo/src/components/prijava.vue
@@ -0,0 +1,21 @@
+<template> 
+    <div>
+        <form action="/action_page.php">
+            <label for="fname">First name:</label><br>
+            <input type="text" id="fname" name="fname" value="John"><br>
+            <label for="lname">Last name:</label><br>
+            <input type="text" id="lname" name="lname" value="Doe"><br><br>
+            <input type="submit" value="Submit">
+        </form> 
+    </div>
+
+</template>
+
+<script>
+export default { }
+
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped> </style>
