fab46244c9691e40bb27ae1e99a7f079b9427a2a
kresogen <37085665+kresogen@users.noreply.github.com>
2020-12-10 17:14:32 +0100

    Create About.vue

---
+++ njuskalo/src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>FILMOVI I SERIJE</h1>
+    <ul>
+      <li>
+        <a href="https://en.wikipedia.org/wiki/Harry_Potter_(film_series)">Harry_Potter</a>
+        <ul>
+          <li><a href="https://en.wikipedia.org/wiki/Harry_Potter_and_the_Philosopher%27s_Stone_(film)">Harry_Potter 1</a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Harry_Potter_and_the_Chamber_of_Secrets_(film)">Harry_Potter 2</a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Harry_Potter_and_the_Prisoner_of_Azkaban_(film)">Harry_Potter 3</a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Harry_Potter_and_the_Goblet_of_Fire_(film)">Harry_Potter 4</a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Harry_Potter_and_the_Order_of_the_Phoenix_(film)">Harry_Potter 5</a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Harry_Potter_and_the_Half-Blood_Prince_(film)">Harry_Potter 6</a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Harry_Potter_and_the_Deathly_Hallows_%E2%80%93_Part_1">Harry_Potter 7</a></li>
+          <li><a href="https://en.wikipedia.org/wiki/Harry_Potter_and_the_Deathly_Hallows_%E2%80%93_Part_2">Harry_Potter 8</a></li>
+        </ul>
+      </li>
+
+    </ul>
+  </div>
+</template>
